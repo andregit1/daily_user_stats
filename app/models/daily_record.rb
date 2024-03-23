a4bcd90533aebb53398afa57912779ab7b3442cf
andregit1 <andreaswidjaya1@gmail.com>
2024-03-23 22:20:39 +0700

    fix after user deletion and count fetch user data

--- app/models/daily_record.rb
+++ app/models/daily_record.rb
@@ -1,6 +1,6 @@
 class DailyRecord < ApplicationRecord
   # Calculates the average age of male and female users
-  before_save :calculate_average_age, if: -> { saved_change_to_male_count? || saved_change_to_female_count? }
+  before_save :calculate_average_age
   
   # Returns the current date in the specified format
   def self.current_date
@@ -8,17 +8,19 @@ class DailyRecord < ApplicationRecord
   end
 
   # Stores the male and female counts in the DailyRecord table
-  def self.store(male_count, female_count)
-    daily_record = find_or_initialize_by(date: current_date)
-    daily_record.assign_attributes(male_count: male_count, female_count: female_count)
+  def self.store(redis_male_count, redis_female_count)
+    daily_record = find_or_initialize_by(date: current_date)  
+    daily_record.male_count = daily_record.male_count_was.to_i + redis_male_count.to_i
+    daily_record.female_count = daily_record.female_count_was.to_i + redis_female_count.to_i
     daily_record.save
   end
+  
 
   # Updates the male and female counts after user deletion
   def self.update_stats_after_user_deletion(user)
     daily_record = find_by(date: current_date)
     
-    if daily_record&.persisted? && daily_record.saved_changes?
+    if daily_record&.persisted?
       if user.gender == 'male'
         daily_record.decrement(:male_count)
       elsif user.gender == 'female'
@@ -27,17 +29,16 @@ class DailyRecord < ApplicationRecord
   
       daily_record.save
     end
-  end  
-  
+  end
+
+  private
+
   # Calculates the average age based on male and female counts
   def calculate_average_age
-    total_male_count = male_count || 0
-    total_female_count = female_count || 0
-
     total_male_age = User.where(gender: 'male').sum(:age)
     total_female_age = User.where(gender: 'female').sum(:age)
 
-    self.male_avg_age = total_male_age.to_f / total_male_count if total_male_count.positive?
-    self.female_avg_age = total_female_age.to_f / total_female_count if total_female_count.positive?
+    self.male_avg_age = total_male_age.to_f / self.male_count if self.male_count.positive?
+    self.female_avg_age = total_female_age.to_f / self.female_count if self.female_count.positive?
   end
 end
